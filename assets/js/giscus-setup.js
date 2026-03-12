fe5cabc8d1f7d2627a5b6b176c2e6e246944871b
IIDA-AILab <52224141+IIDA-AILab@users.noreply.github.com>
2026-03-12 02:40:44 +0000

    Deploying to gh-pages from @ IIDA-AILab/iida.github.io@dbfa02ea8d0ed4f6b6a94df85bf609e333e91a03 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "IIDA-AILab/iida-lab",
+    "data-repo": "IIDA-AILab/iida.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
